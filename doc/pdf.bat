0002240f11603e88c7fa8fb05d379edc2c271353
bear <bear@prosoftsystems.ru>
2014-08-12 16:14:11 +0600

    Со сборочного чертежа убраны элементы которые никогда не запаиваются.
    Signed-off-by: bear <bear@prosoftsystems.ru>

--- doc/pdf.bat
+++ doc/pdf.bat
@@ -9,11 +9,6 @@ chcp 65001
 SET PDFSAM=java -Dlog4j.configuration=console-log4j.xml -jar %PDFSAM_CONSOLE%\pdfsam-console-2.4.1e.jar
 :: ключи для запуска консольной версии приложения pdfsam
 SET OPTIONS=-overwrite -compressed -pdfversion "5" concat
-:: путь к общей папке документации
-:: SET GLBPCKG="Z:\Текущие версии железа\Блоки АВАНТ"
-
-:: копирование файлов
-::goto COPY_TO_GLBPCKG
 
 :: -----     СОЗДАНИЕ ФАЙЛОВ PDF     -----
 :: || - следующая команда за этим знаком выполняется только если предыдущая завершилась с ошибкой
@@ -21,21 +16,6 @@ SET OPTIONS=-overwrite -compressed -pdfversion "5" concat
 
 :: БВП Р400м, РЗСК
 %PDFSAM% -l ".\files.xml" -o ".\Плата КВП KVP_03v2.pdf" %OPTIONS% || goto EXIT_ERROR
-goto EXIT_NO_ERROR
-
-:: -----     КОПИРОВАНИЕ В ОБЩУЮ ПАПКУ     -----
-:COPY_TO_GLBPCKG
-
-:: /I - копирование с созданием необходимых папок
-:: /Y - замена существующих файлов без запроса
-@echo.
-@echo Copy ".\Documents\" to %GLBPCKG.
-@echo.
-xcopy ".\Documents" %GLBPCKG% /I /Y		|| goto EXIT_ERROR			
-@echo.
-@echo Copy "readme.txt" to %GLBPCKG.
-@echo.
-xcopy "readme.txt" %GLBPCKG% /Y			|| goto EXIT_ERROR
 
 :: -----     ЗАВЕРШЕНИЕ РАБОТЫ     -----
 
@@ -51,7 +31,7 @@ goto :EXIT
 @echo. 
 @echo !!! FAIL !!!
 @echo.
-CMD /Q /K
+PAUSE
 goto EXIT
 
 :EXIT
